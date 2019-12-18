e2a24154bf2f77fa9fd0612da04bf7fcb6cd080d
imfine0104 <vuongtran0104@gmail.com>
2019-12-18 15:48:05 +0900

    eat together

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ var Cat = require('./cat');
 var Mouse = require('./mouse');
 var Dog = require('./dog');
 
+var bin	= new Dog('pun');
 var tom = new Cat('tomy');
 var jerry = new Mouse('micky');
 
+console.log(bin);
+console.log(tom);
+console.log(jerry);
+console.log('tomy eat micky ');
 tom.eat(jerry);
 console.log(tom);
-var bin	= new Dog();
 bin.eat(tom);
+console.log('pun eat micky ');
 console.log(bin);
 
 
